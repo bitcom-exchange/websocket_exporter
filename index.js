72ab3a0f2df28f1ec964a65cbe36d533941af9f7
zkvmpb <zhong.zhuang@antfin.com>
2021-03-04 13:53:48 +0800

    support endpoint array

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const server = require('express')();
 const client = require('prom-client');
 const { WebSocketClient } = require('./websocket');
-const ENDPOINT = process.env.ENDPOINT
+const ENDPOINTS = process.env.ENDPOINTS.split(" ")
 
 const gauge = new client.Gauge({
-  name: 'websocket',
+  name: 'websocket_status',
   help: 'websocket_help',
-  labelNames: ['status']
+  labelNames: ["endpoint"]
 });
 
-const ws = new WebSocketClient();
-ws.open(ENDPOINT);
-ws.onopen = function (e) {
-  gauge.labels('status').set(1);
-}
-ws.onerror = function (e) {
-  gauge.labels('status').set(0);
+for (let i=0; i < ENDPOINTS.length; i++) {
+  const endpoint = ENDPOINTS[i]
+  const ws = new WebSocketClient();
+  ws.open(endpoint);
+  ws.onopen = function (e) {
+    gauge.labels(endpoint).set(1);
+  }
+  ws.onerror = function (e) {
+    gauge.labels(endpoint).set(0);
+  }
 }
 
 server.get('/metrics', (req, res) => {
